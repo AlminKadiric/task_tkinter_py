b02bdea9a5ccfd2dd978b4a240d69c0a2c1d6872
almin <alminkadiric7@gmail.com>
2024-08-03 23:15:14 +0200

    edit

--- anime.py
+++ anime.py
@@ -5,5 +5,8 @@ canvas = tk.Canvas(root,width=100,height=100)
 canvas.pack()
 circle = canvas.create_oval(50,50,80,80,outline="white",fill="blue")
 
-
+def redraw():
+    canvas.after(50,redraw)
+    canvas.move(circle,155,155)
+    print(canvas.coords(circle))
 root.mainloop()
